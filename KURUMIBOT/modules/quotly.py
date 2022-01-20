b20512ae8e757e35765052fbba60e0a1f9b2c6e7
BOUNTYHUNTER13 <88586390+BOUNTYHUNTER13@users.noreply.github.com>
2022-01-20 19:44:03 +0530

    Update quotly.py

--- KURUMIBOT/modules/quotly.py
+++ KURUMIBOT/modules/quotly.py
@@ -1,42 +1,8 @@
-"""
-MIT License
-
-Copyright (c) 2021 TheHamkerCat
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
 from io import BytesIO
 from traceback import format_exc
-
 from pyrogram import filters
 from pyrogram.types import Message
-
-from wbb import SUDOERS, USERBOT_PREFIX, app, app2, arq
-from wbb.core.decorators.errors import capture_err
-
-MODULE = "Quotly"
-HELP = """
-/q - To quote a message.
-/q [INTEGER] - To quote more than 1 messages.
-/q r - to quote a message with it's reply
-
-"""
+from KURUMIBOT import pbot, arq
 
 
 async def quotify(messages: list):
@@ -54,7 +20,7 @@ def getArg(message: Message) -> str:
     return arg
 
 
-def isArgInt(message: Message) -> list:
+def isArgInt(message: Message) -> bool:
     count = getArg(message)
     try:
         count = int(count)
@@ -63,19 +29,17 @@ def isArgInt(message: Message) -> list:
         return [False, 0]
 
 
-@app2.on_message(
-    filters.command("q") & filters.user(SUDOERS)
-)
-@app.on_message(filters.command("q") & ~filters.private)
-@capture_err
+@pbot.on_message(filters.command("q"))
 async def quotly_func(client, message: Message):
     if not message.reply_to_message:
-        return await message.reply_text("Reply to a message to quote it.")
+        return await message.reply_text(
+            "Reply to a message to quote it."
+        )
     if not message.reply_to_message.text:
         return await message.reply_text(
             "Replied message has no text, can't quote it."
         )
-    m = await message.reply_text("Quoting Messages")
+    m = await message.reply_text("Quoting Messages Please wait....")
     if len(message.command) < 2:
         messages = [message.reply_to_message]
 
@@ -84,28 +48,22 @@ async def quotly_func(client, message: Message):
         if arg[0]:
             if arg[1] < 2 or arg[1] > 10:
                 return await m.edit("Argument must be between 2-10.")
-
             count = arg[1]
-
-            # Fetching 5 extra messages so tha twe can ignore media
-            # messages and still end up with correct offset
-            messages = [
-                i
-                for i in await client.get_messages(
-                    message.chat.id,
-                    range(
+            messages = await client.get_messages(
+                message.chat.id,
+                [
+                    i
+                    for i in range(
                         message.reply_to_message.message_id,
-                        message.reply_to_message.message_id + (count + 5),
-                    ),
-                    replies=0,
-                )
-                if not i.empty and not i.media
-            ]
-            messages = messages[:count]
+                        message.reply_to_message.message_id + count,
+                    )
+                ],
+                replies=0,
+            )
         else:
             if getArg(message) != "r":
                 return await m.edit(
-                    "Incorrect Argument, Pass 'r' or 'INT', EX: /q 2"
+                    "Incorrect Argument, Pass **'r'** or **'INT'**, **EX:** __/q 2__"
                 )
             reply_message = await client.get_messages(
                 message.chat.id,
@@ -114,13 +72,11 @@ async def quotly_func(client, message: Message):
             )
             messages = [reply_message]
     else:
-        return await m.edit(
+        await m.edit(
             "Incorrect argument, check quotly module in help section."
         )
+        return
     try:
-        if not message:
-            return await m.edit("Something went wrong.")
-
         sticker = await quotify(messages)
         if not sticker[0]:
             await message.reply_text(sticker[1])
@@ -131,10 +87,9 @@ async def quotly_func(client, message: Message):
         sticker.close()
     except Exception as e:
         await m.edit(
-            "Something went wrong while quoting messages,"
+            "Something wrong happened while quoting messages,"
             + " This error usually happens when there's a "
-            + " message containing something other than text,"
-            + " or one of the messages in-between are deleted."
+            + " message containing something other than text."
         )
         e = format_exc()
         print(e)
